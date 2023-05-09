19d9c15a1efa26b01ff3f66a9cf3a6b2165594c3
Christoph33one <cjbrowning23@hotmail.com>
2023-05-09 14:44:32 +0000

    added a favicon, navbar with content and responsive layout,  website icon

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from '../assets/logo.png';
+
+const NavBar = () => {
+  return <Navbar bg="dark" variant="dark" expand="md" fixed="top">
+  <Container>
+    <Navbar.Brand><img src={logo} alt="logo" height="45" />
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="ml-auto text-uppercase align-items-start ">
+        <Nav.Link><i className="fas fa-home"></i>Home</Nav.Link>
+        <Nav.Link><i className="fas fa-search-location"></i>Locations</Nav.Link>
+        <Nav.Link><i className="fas fa-sign-in-alt"></i>Sign in</Nav.Link>
+        <Nav.Link><i className="fas fa-user-plus"></i>Sign up</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+
+}
+
+export default NavBar;
