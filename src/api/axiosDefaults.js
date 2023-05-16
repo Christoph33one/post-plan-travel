fb3e2d23034b10185b4898fbbe9a3ca327ab1a79
Christoph33one <cjbrowning23@hotmail.com>
2023-05-16 15:49:53 +0000

    added interceptors to CurrentUserContext for token fresh times

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://travel-planner-api.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true
+axios.defaults.baseURL = "https://travel-planner-api.herokuapp.com/";
+axios.defaults.headers.post['Content-Type'] = "multipart/form-data";
+axios.defaults.withCredentials = true;
+
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
