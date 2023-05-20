810fa47767f26e52a96482884d1a9ca951bf2e8d
Christoph33one <cjbrowning23@hotmail.com>
2023-05-20 19:13:24 +0000

    added homepage feature links and new colour design for better UX

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -5,7 +5,6 @@ import styles from '../styles/HomePage.module.css'
 const HeroImageComponent = () => {
   return (
     <div className={styles.HomePage}>
-      <h1>POST, PLAN & TRAVEL</h1>
       <div className={styles.HeroImage}>
         <img src={HeroImageSrc} alt="Hero Image" className={styles.HeroImageImg} />
       </div>
--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 import HeroImageComponent from '../../components/HeroImage';
+import { Container, Button } from 'react-bootstrap';
+import styles from '../../styles/HomePage.module.css'
 
 
 const HomePage = () => {
+  const categories = [
+    { title: 'Car Rental', link:
+     'https://www.rentalcars.com/?affiliateCode=google_brand&preflang=en&label=rentalcars.com-brand-QMBxf9tHoSbRGoWKQmhm3gS282611385565&ws=&ppc_placement=&ppc_target=&ppc_param1=&ppc_param2=&aceid=&adposition=&ppc_network=g&feeditemid=&ppc_targetid=kwd-29977620&loc_physical_ms=1010561&loc_interest_ms=&ppc_device=c&ppc_devicemodel=&gclid=CjwKCAjw36GjBhAkEiwAKwIWyWcxVJ3HYNlOnshdMW4leffRVdyxHQauQib7e6KGuNgV3POmpbpQtxoCQEAQAvD_BwE' },
+    { title: 'Flights', link: 'https://www.skyscanner.net/' },
+    { title: 'Safety', link: 'https://www.betterhealth.vic.gov.au/health/healthyliving/travel-safety-tips' },
+    { title: 'Tech', link: 'https://www.travelandleisure.com/travel-tips/cool-gadgets/essential-travel-tech-gear' },
+    { title: 'Equipment', link: 'https://www.tradeinn.com/trekkinn/en' },
+  ];
+
+
+
   return (
     <div>
+       <h1>POST, PLAN & TRAVEL</h1>
+       <Container>
+      <div className={styles.boxContainer}>
+        {categories.map((category, index) => (
+          <div className={styles.box} key={index}>
+            <i className="fas fa-map"></i>
+            <a href={category.link} target="_blank">{category.title}</a>
+          </div>
+        ))}
+      </div>
+      
+    </Container>
       <HeroImageComponent />
       <Container fluid>
         <h3>
@@ -17,7 +41,9 @@ const HomePage = () => {
           in the joy of travel!
         </h3>
       </Container>
+     
     </div>
+    
   );
 };
 
