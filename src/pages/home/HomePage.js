64d12bc4a9c9816285db1b9d5b363b5cdeebe16c
Christoph33one <cjbrowning23@hotmail.com>
2023-05-21 22:03:00 +0000

    added donations card to Homage for added  site content

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import HeroImageComponent from '../../components/HeroImage';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import styles from '../../styles/HomePage.module.css'
 
 
@@ -31,16 +31,33 @@ const HomePage = () => {
     </Container>
       <HeroImageComponent />
       <Container fluid>
-        <h3>
+        <h4>
           Welcome to Post, Plan & Travel! 
           Get ready for an exhilarating adventure as we take you on a whirlwind tour of the world. 
           Explore a treasure trove of travel plans crafted by passionate globetrotters. 
           Join our vibrant community and let your wanderlust ignite. Embark on a journey of limitless
           possibilities and create extraordinary memories. Unleash the explorer within you and indulge
           in the joy of travel!
-        </h3>
+        </h4>
       </Container>
-     
+      <Container className={styles.container}>
+      <Row>
+        <Col md={6}>
+          <div className={styles.imageWrapper}>
+            <img src="signup-logo.jpg" alt="Image" className={styles.image} />
+          </div>
+        </Col>
+        <Col md={6} className={styles.content}>
+          <h2>Help us help the planet</h2>
+          <h5>Together, we’ve raised more than $10 million USD for World Central Kitchen’s
+            emergency food relief efforts around the world. Donate to their Climate Disaster
+            Fund and the Tripadvisor Foundation will match it.</h5>
+          <Button variant="dark" 
+          href="https://donate.wck.org/give/480256/#!/donation/checkout"
+          target="_blank">DONATE</Button>
+        </Col>
+      </Row>
+    </Container>
     </div>
     
   );
