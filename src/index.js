302cc82112e6276c64f0ea253d820b715afb4cf9
Christoph33one <cjbrowning23@hotmail.com>
2023-05-27 15:12:57 +0000

    context/ProfileDataContext.js - Sync retrieved  profile data across all components

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
-import { CurrentUserProvider } from './contexts/CurrentUserContext';
+import { CurrentUserProvider } from "./contexts/CurrentUserContext";
+import { ProfileDataProvider } from "./contexts/ProfileDataContext";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <CurrentUserProvider>
-      <App />
+        <ProfileDataProvider>
+          <App />
+        </ProfileDataProvider>
       </CurrentUserProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -1,48 +1,52 @@
-import React from 'react'
-import styles from '../../styles/Profile.module.css';
-import btnStyles from '../../styles/Button.module.css';
-import { useCurrentUser } from '../../contexts/CurrentUserContext';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import React from "react";
+import styles from "../../styles/Profile.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
-import { Button } from 'react-bootstrap';
+import { Button } from "react-bootstrap";
 
 const Profile = (props) => {
-    const {profile, mobile, imageSize=55} = props
-    const {id, following_id, image, owner} = profile
+  const { profile, mobile, imageSize = 55 } = props;
+  const { id, following_id, image, owner } = profile;
 
-     const currentUser = useCurrentUser();
-     const is_owner = currentUser?.username === owner;
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
 
   return (
-     <div 
-        className={`my-3 d-flex align-items-center ${mobile && "flex-cloumn"}`}
-     >  
-        <div>
-            <Link className="align-self-center" to={`/profiles/${id}`}>
-                <Avatar src={image} height={imageSize} />
-            </Link>
-        </div>
-            <div className={`mx-2 ${styles.WordBreak}`}>
-                <strong>{owner}</strong>
-            </div>
-            <div className={`text-right ${!mobile && 'ml-auto'}`}>
-                {!mobile && currentUser && !is_owner && (
-                    following_id ? (
-                        <Button className={`${btnStyles.Button} ${btnStyles.BlackOutLine}`}
-                        onClick={() => {}}
-                        >
-                        unfollow</Button>
-                    ) : (
-                        <Button className={`${btnStyles.Button} ${btnStyles.BlackOutLine}`}
-                        onClick={() => {}}
-                        >
-                        follow</Button>
-                    )
-                )}
-            </div>
+    <div
+      className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
+    >
+      <div>
+        <Link className="align-self-center" to={`/profiles/${id}`}>
+          <Avatar src={image} height={imageSize} />
+        </Link>
+      </div>
+      <div className={`mx-2 ${styles.WordBreak}`}>
+        <strong>{owner}</strong>
+      </div>
+      <div className={`text-right ${!mobile && "ml-auto"}`}>
+        {!mobile &&
+          currentUser &&
+          !is_owner &&
+          (following_id ? (
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.BlackOutline}`}
+              onClick={() => {}}
+            >
+              unfollow
+            </Button>
+          ) : (
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Black}`}
+              onClick={() => {}}
+            >
+              follow
+            </Button>
+          ))}
+      </div>
+    </div>
+  );
+};
 
-     </div>
-  )
-}
-
-export default Profile
+export default Profile;
