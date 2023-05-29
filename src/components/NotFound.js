2cd2e2081d8b27c74accad8e85af7351b8cecf4f
Christoph33one <cjbrowning23@hotmail.com>
2023-05-29 21:17:20 +0000

    components/NotFound.js - invalid url defualt image added

---
+++ src/components/NotFound.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NoResults from "../assets/NoResults.jpg";
+import styles from "../styles/NotFound.module.css";
+import Asset from "../components/Asset";
+
+const NotFound = () => {
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={NoResults} alt="No Results" />
+        <div className={styles.message}>
+          Sorry, the page you're looking for doesn't exist
+        </div>
+      </div>
+    );
+  };
+  
+  export default NotFound;
