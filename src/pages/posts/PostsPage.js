8a07118128c9e3060bc757adaaa30dd3bc606e92
Christoph33one <cjbrowning23@hotmail.com>
2023-05-30 15:42:10 +0000

    post/PostsPage.js - refected posts on user login/logout

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -17,6 +17,7 @@ import NoResults from "../../assets/no-results.png";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
 import PopularProfiles from "../profiles/PopularProfiles";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 
 function PostsPage({ message, filter = "" }) {
@@ -25,6 +26,7 @@ function PostsPage({ message, filter = "" }) {
   const { pathname } = useLocation();
 
   const [query, setQuery] = useState("");
+  const currentUser = useCurrentUser();
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -45,7 +47,7 @@ function PostsPage({ message, filter = "" }) {
       clearTimeout(timer);
     };
   
-  }, [filter, query, pathname]);
+  }, [filter, query, pathname, currentUser]);
 
   return (
     <Row className="h-100">
