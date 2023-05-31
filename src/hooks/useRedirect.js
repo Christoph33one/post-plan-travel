8cd352a9315f5b2a85fcf98859086af23b561852
Christoph33one <cjbrowning23@hotmail.com>
2023-05-31 16:00:29 +0000

    PostcreateForm - fixed bug redirect loggout user from create post page

--- src/hooks/useRedirect.js
+++ src/hooks/useRedirect.js
@@ -10,12 +10,12 @@ export const useRedirect = (userAuthStatus) => {
       try {
         await axios.post('/dj-rest-auth/token/refresh/')
          // if user is logged in, the code below will run
-         if (userAuthStatus === 'loggedIn'){
+         if (userAuthStatus === "loggedIn"){
             history.push('/')
          }
         } catch (err) {
             // if user is not logged in, the code below will run
-            if (userAuthStatus === 'loggedOut'){
+            if (userAuthStatus === "loggedOut"){
                 history.push('/')
             }
         }
--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -9,7 +9,7 @@ import Image from "react-bootstrap/Image";
 import Asset from "../../components/Asset";
 import Upload from "../../assets/upload.png";
 import styles from "../../styles/PostCreateEditForm.module.css";
-import appStyles from "../../App.module.css";
+import appStyles from "../../App.module.css"; 
 import btnStyles from "../../styles/Button.module.css";
 import { useHistory } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
